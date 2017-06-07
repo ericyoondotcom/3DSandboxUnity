93ff3d9f9e91653cea01a3094fe6dec620634d30
Eric <emjyoon@gmail.com>
2017-06-06 17:14:59 -0700

    Add commands (kind of)

---
+++ 3D Sandbox Game/Assets/Scripts/Commands.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(InputField))]
+public class Commands : MonoBehaviour {
+	InputField ipf;
+
+	// Use this for initialization
+	void Start () {
+		ipf = GetComponent<InputField> ();
+	}
+
+
+	public void EnterCommand(){
+		string text = ipf.text;
+		char[] chars = { ' ' };
+		List<string> parameters = new List<string> (text.Split(chars));
+		switch (parameters [0]) {
+		case "":
+			break;
+			case "/hi":
+			case "/hello":
+				ipf.text = "Hey! What's up?";
+				break;
+		case "/bye":
+		case "/goodbye":
+			ipf.text = "Goodbye!";
+			break;
+		default:
+			ipf.text = "Command not found";
+			break;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 3D Sandbox Game/Assets/Scripts/FallDamage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDamage : MonoBehaviour {
+	//linear eqns!
+	public float slope;
+	public float yIntercept;
+
+	float initY;
+	PlayerHealth ph;
+	// Use this for initialization
+	void Start () {
+		initY = transform.position.y;
+		ph = GetComponent<PlayerHealth> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collision coll){
+		//Debug.Log ("collided");
+		float distance = initY - transform.position.y;
+		Debug.Log (distance);
+		if (distance > 0) {
+			float damage = (slope * distance) + yIntercept;
+			if (damage > 0) {
+				ph.health -= (int)damage;
+			}
+		}
+		initY = transform.position.y;
+	}
+}
